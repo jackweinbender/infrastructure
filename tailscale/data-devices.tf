9323a865cf5615ccab9229c9885549b581691642
Jack Weinbender <jack@weinbender.io>
2024-09-29 18:37:30 -0500

    Add lapis to TS

--- tailscale/data-devices.tf
+++ tailscale/data-devices.tf
@@ -12,3 +12,8 @@ data "tailscale_device" "jack-iphone" {
   name     = "jack-iphone.tortoise-noodlefish.ts.net"
   wait_for = "60s"
 }
+
+data "tailscale_device" "lapis" {
+  name     = "lapis.tortoise-noodlefish.ts.net"
+  wait_for = "60s"
+}
