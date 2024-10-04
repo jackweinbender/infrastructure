871e684c9cae50b02dbbc84c4a883ae5c4358064
Jack Weinbender <jack@weinbender.io>
2024-10-03 21:12:23 -0500

    Update certbot script

--- config/certbot/update.sh
+++ config/certbot/update.sh
@@ -1,18 +1,18 @@
+# Script to update the certbot config and run the certbot
+# cert ACME challenge stuff
+
+THIS_CONFIG_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+CERTBOT_CONFIG_DIR="/etc/certbot"
+
 # Gotta make sure the directory exists
-sudo mkdir -p /etc/certbot
+sudo mkdir -p $CERTBOT_CONFIG_DIR
 
 # Generate the ini files with secrets
-op inject -i cloudflare.ini.tmpl -o cloudflare.ini
-sudo mv cloudflare.ini /etc/certbot/cloudflare.ini
+op inject -i $THIS_CONFIG_DIR/cloudflare.ini.tmpl -o cloudflare.ini
+sudo mv cloudflare.ini $CERTBOT_CONFIG_DIR/cloudflare.ini
 
 sudo certbot certonly \
   --dns-cloudflare \
   --dns-cloudflare-propagation-seconds 30 \
-  --dns-cloudflare-credentials /etc/certbot/cloudflare.ini \
-  -d *.home.weinbender.io
-
-sudo certbot certonly \
-  --dns-cloudflare \
-  --dns-cloudflare-propagation-seconds 30 \
-  --dns-cloudflare-credentials /etc/certbot/cloudflare.ini \
+  --dns-cloudflare-credentials $CERTBOT_CONFIG_DIR/cloudflare.ini \
   -d *.home.weinbender.io
