1b96253d339a169a9871d3b13bad4cfe80c0df3e
Jack Weinbender <jack@weinbender.io>
2024-11-08 12:56:35 -0600

    Ansible PR workflow (#13)
    * Ansible PR workflow
    
    * Ansible PR workflow
    
    * Move around
    
    * Update check
    
    * Update path

--- config/certbot/update_cert.sh
+++
@@ -1,7 +0,0 @@
-CERTBOT_CONFIG_DIR="/etc/certbot"
-
-sudo certbot certonly \
-  --dns-cloudflare \
-  --dns-cloudflare-propagation-seconds 30 \
-  --dns-cloudflare-credentials $CERTBOT_CONFIG_DIR/cloudflare.ini \
-  -d *.home.weinbender.io
