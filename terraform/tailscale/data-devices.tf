2565d72e373bd5cf593e2eebd2f691abc06f5f3b
Jack Weinbender <jack@weinbender.io>
2024-11-29 16:49:44 -0600

    use https

--- terraform/tailscale/data-devices.tf
+++ terraform/tailscale/data-devices.tf
@@ -1,13 +1,3 @@
-data "tailscale_device" "ada" {
-  name     = "ada.tortoise-noodlefish.ts.net"
-  wait_for = "60s"
-}
-
-data "tailscale_device" "mba" {
-  name     = "mba.tortoise-noodlefish.ts.net"
-  wait_for = "60s"
-}
-
 data "tailscale_device" "jack-iphone" {
   name     = "jack-iphone.tortoise-noodlefish.ts.net"
   wait_for = "60s"
