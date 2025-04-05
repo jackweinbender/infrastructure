27c2fa98965d118d65c735e8b0502d741b754d7b
Jack Weinbender <jack@weinbender.io>
2025-04-04 22:35:24 -0500

    More

--- kubernetes/bootstrap/microk8s.sh
+++ kubernetes/bootstrap/microk8s.sh
@@ -7,6 +7,3 @@ microk8s enable dns
 # `ingress` here is the ingress-nginx controller, which is an official Kubernetes
 # project that manages external access to the services in a cluster.
 microk8s enable ingress
-
-# `cert-manager` automates certs with letsencrypt
-microk8s enable cert-manager
