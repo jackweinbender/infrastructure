7a2624dfe8d838448c73764babfc5274567841de
Jack Weinbender <jack@weinbender.io>
2025-05-09 21:19:10 -0500

    add envoy

--- kubernetes/bootstrap/microk8s.sh
+++ kubernetes/bootstrap/microk8s.sh
@@ -1,6 +1,7 @@
 # Commands to bootstrap microk8s on a new host.
 # These commands are idempotent, so you can run them multiple times without issues.
 alias kubectl='microk8s kubectl'
+alias k='microk8s kubectl'
 
 # DNS is required for most applications to resolve service names within the cluster.
 # in my case, argocd is the key app that needs this
