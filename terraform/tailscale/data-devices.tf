e94864f4e55f2275f15fa0466d80cd7e60f0a327
Jack Weinbender <jack@weinbender.io>
2025-07-05 06:09:02 -0700

    tf

--- terraform/tailscale/data-devices.tf
+++
@@ -1,9 +0,0 @@
-data "tailscale_device" "jack-iphone" {
-  name     = "jack-iphone.tortoise-noodlefish.ts.net"
-  wait_for = "60s"
-}
-
-data "tailscale_device" "lapis" {
-  name     = "lapis.tortoise-noodlefish.ts.net"
-  wait_for = "60s"
-}
