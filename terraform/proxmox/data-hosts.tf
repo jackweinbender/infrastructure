00e429457d1f699ab5ce53271a61a552bba65afc
Jack Weinbender <jack@weinbender.io>
2025-07-04 21:45:25 -0700

    proxmox hosts

--- terraform/proxmox/data-hosts.tf
+++ terraform/proxmox/data-hosts.tf
@@ -1,14 +1,3 @@
-data "proxmox_virtual_environment_hosts" "ada-host" {
-  node_name = "ada-host"
-}
-# data "proxmox_virtual_environment_hosts" "caba-host" {
-#   node_name = "caba-host"
-# }
-
-data "proxmox_virtual_environment_hosts" "gog-host" {
-  node_name = "gog-host"
-}
-
-data "proxmox_virtual_environment_hosts" "magog-host" {
-  node_name = "magog-host"
+data "proxmox_virtual_environment_hosts" "caba-host" {
+  node_name = "caba-host"
 }
