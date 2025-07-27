34d295ac66dbf8e5d45ed515bad7efc6791abb49
Jack Weinbender <jack@weinbender.io>
2025-07-26 21:31:36 -0500

    GO

--- kubernetes/postgresql/config/init.sql
+++ kubernetes/postgresql/config/init.sql
@@ -1,7 +1,4 @@
 -- Create pgvector extension
 CREATE EXTENSION IF NOT EXISTS vector;
--- Create a sample database for testing
-CREATE DATABASE app_db;
--- Connect to the new database and enable vector extension there too
-\c app_db;
-CREATE EXTENSION IF NOT EXISTS vector;
+-- Create Twenty CRM database
+CREATE DATABASE IF NOT EXISTS twentycrm;
