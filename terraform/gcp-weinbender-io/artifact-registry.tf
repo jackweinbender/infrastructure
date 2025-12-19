cd0a66abf25a3a4f6079141ec01e6b6be9d0c2be
Jack Weinbender <jack@weinbender.io>
2025-12-18 23:43:48 -0600

    clean apply

--- terraform/gcp-weinbender-io/artifact-registry.tf
+++ terraform/gcp-weinbender-io/artifact-registry.tf
@@ -12,27 +12,16 @@ resource "google_artifact_registry_repository" "docker_nonprod" {
   format        = "DOCKER"
 }
 
-# ServiceAccounts for Cloud Run to pull from Artifact Registry
-data "google_service_account" "remindme_cloudrun_robot" {
-  account_id = "remind-me-481700"
-  name       = "service-13264350760@serverless-robot-prod.iam.gserviceaccount.com"
-}
-locals {
-  pull_sa_members = [
-    data.google_service_account.remindme_cloudrun_robot.member
-  ]
-}
 resource "google_artifact_registry_repository_iam_member" "cloudrun_reader_releases" {
-  for_each   = toset(local.pull_sa_members)
   location   = google_artifact_registry_repository.docker_releases.location
   repository = google_artifact_registry_repository.docker_releases.repository_id
   role       = "roles/artifactregistry.reader"
-  member     = each.value
+  member     = "serviceAccount:service-13264350760@serverless-robot-prod.iam.gserviceaccount.com"
 }
+
 resource "google_artifact_registry_repository_iam_member" "cloudrun_reader_nonprod" {
-  for_each   = toset(local.pull_sa_members)
   location   = google_artifact_registry_repository.docker_nonprod.location
   repository = google_artifact_registry_repository.docker_nonprod.repository_id
   role       = "roles/artifactregistry.reader"
-  member     = each.value
+  member     = "serviceAccount:service-13264350760@serverless-robot-prod.iam.gserviceaccount.com"
 }
