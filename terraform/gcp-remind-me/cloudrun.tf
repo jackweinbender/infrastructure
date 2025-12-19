02530ea42d4fd763562ec4d889e25cf0de4ff400
Jack Weinbender <jack@weinbender.io>
2025-12-18 21:13:27 -0600

    Allow all users view

--- terraform/gcp-remind-me/cloudrun.tf
+++ terraform/gcp-remind-me/cloudrun.tf
@@ -14,3 +14,10 @@ resource "google_cloud_run_v2_service" "remindme" {
     }
   }
 }
+
+resource "google_cloud_run_service_iam_member" "public_access" {
+  service  = google_cloud_run_v2_service.remindme.name
+  location = google_cloud_run_v2_service.remindme.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
